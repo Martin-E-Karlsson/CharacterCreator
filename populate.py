544e67184da82bce441fce8e1553dccaf55c3270
Martin Karlsson <m.karlsson91@gmail.com>
2020-10-05 17:48:31 +0200

    populate.py received basic functionality

--- populate.py
+++ populate.py
@@ -1,50 +1,49 @@
 import json
+# import PySimpleGUI as sg
+
+
+def add_value(json_data, path, value):
+    for key in path[:-1]:
+        json_data = json_data.setdefault(key, {})
+    json_data[path[-1]] = value
+
+    return json_data
+
+
+def add_values_classes(json_data, path):
+    for key in json_data["Class"].keys():
+        values_list = []
+        count = 0
+        while True:
+            count += 1
+            new_value = input(f"{key} {path[-1]} Value #{count}: ")
+            if new_value != "":
+                values_list.append(new_value)
+            else:
+                break
+
+        add_value(json_data, ["Class", key, path[0]], {path[1]: values_list})
+
+    return json_data
+
+
+def fetch_value(data, keys):
+    return fetch_value(data[keys[0]], keys[1:]) \
+        if keys else data
+
+
+def run():
+    with open('dnd5e.json') as f:
+        data_in = json.load(f)
+
+    data_out = add_values_classes(data_in, ["Proficiencies", "Armor"])
+
+    with open('dnd5e.json', 'w') as f:
+        json.dump(data_out, f, indent=2, sort_keys=True)
 
 
 def main():
-    # classes_string = '''
-    # {
-    #     "classes": [
-    #         "Barbarian",
-    #         "Bard",
-    #         "Cleric",
-    #         "Druid",
-    #         "Fighter",
-    #         "Monk",
-    #         "Paladin",
-    #         "Ranger",
-    #         "Rogue",
-    #         "Sorcerer",
-    #         "Warlock",
-    #         "Wizard"
-    #     ]
-    # }
-    # '''
-
-    with open('dnd5e.json') as f:
-        data = json.load(f)
-
-    # data = json.loads(classes_string)
-    # print(data)
-    # print(type(data))
-
-    # for i, c in enumerate(data['classes']):
-    #     data['classes'][i] = 'Gnome ' + c
-    #
-    # new_string = json.dumps(data, indent=2)
-    #
-    # print(new_string)
-    #
-    # with open('gnome_classes.json', 'w') as f:
-    #     json.dump(data, f, indent=2)
-
-    for c in data['classes']:
-        # print(f"{c} has the hit die {data['classes'][c]['Hit Die']}")
-        # data['classes'][c]['Saves'] = ['Strength', 'Constitution']
-        data['classes'][c]['Saves'].sort()
-
-    with open('dnd5e.json', 'w') as f:
-        json.dump(data, f, indent=2, sort_keys=True)
+    run()
 
 
 if __name__ == '__main__':
